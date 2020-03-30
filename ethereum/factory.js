4efd36e200f879b91ce4b9baf421dd3800ecbf77
jeremy.zhang <zhangyue9306@gmail.com>
2020-03-30 11:33:12 +0800

    get factory instance

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -2,6 +2,6 @@ import web3 from "./web3";
 import PropositionFactory from './build/PropositionFactory';
 
 const contractAddress = '0xA99ed7D4dC171691ccBe9129D3010d1D26624fa7';
-const factory = web3.eth.Contract(JSON.parse(PropositionFactory.interface), contractAddress);
+const factory = new web3.eth.Contract(JSON.parse(PropositionFactory.interface), contractAddress);
 
 export default factory;
