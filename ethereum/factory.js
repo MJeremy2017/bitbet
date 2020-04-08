4becbcd06b6661b862ae2583889fd663352d8722
jeremy.zhang <zhangyue9306@gmail.com>
2020-04-08 12:17:39 +0800

    change address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,7 +1,7 @@
 import web3 from "./web3";
 import PropositionFactory from './build/PropositionFactory';
 
-const contractAddress = '0xA99ed7D4dC171691ccBe9129D3010d1D26624fa7';
+const contractAddress = '0x5588EE6e0eaA3C56e3385f573a3fdAcE9267ae72';
 const factory = new web3.eth.Contract(JSON.parse(PropositionFactory.interface), contractAddress);
 
 export default factory;
